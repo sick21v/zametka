63c7a5605fd3b7c8d4a7d70864039d28251997c5
sarum0 <90762376+sick21v@users.noreply.github.com>
2026-05-27 19:12:30 +0300

    Add files via upload

---
+++ notes_manager.py
@@ -0,0 +1,35 @@
+from delete_note import delete_note
+from edit_note import edit_note
+
+
+def main():
+    n = 0
+    while True:
+        print("Выберите:\n"
+              "1. Добавить заметку\n"
+              "2. Показать все\n"
+              "3. Выполнить заметку\n"
+              "4. Удалить заметку\n"
+              "5. Редактировать\n"
+              "6. Найти заметки\n"
+              "7. Выйти")
+        n = input()
+        if n == "1":
+            add_note
+        if n == "2":
+            show_all
+        if n == "3":
+            open_note
+        if n == "4":
+            delete_note
+        if n == "5":
+            edit_note
+        if n == "6":
+            find_note
+        if n == '7':
+            break
+        else:
+            print("Пока не реализовано")
+
+if __name__ == '__main__':
+    main()
